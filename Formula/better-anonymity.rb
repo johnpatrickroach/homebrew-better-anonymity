706199c0d75bf3d864705d41452d2c8e060b2f95
johnpatrickroach <johnpatrickroach1@gmail.com>
2026-01-06 15:37:51 -0800

    feat: Add better-anonymity v1.0.0 formula

---
+++ Formula/better-anonymity.rb
@@ -0,0 +1,36 @@
+class BetterAnonymity < Formula
+  desc "MacOS Security, Privacy & Anonymity Tools"
+  homepage "https://github.com/phaedrus/better-anonymity"
+  url "https://github.com/johnpatrickroach/better-anonymity/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "cf76f0547b5e91a2e49b4f23266769e3459d1218778b08d9beeafdf6bdd15229"
+  license "MIT"
+  head "https://github.com/phaedrus/better-anonymity.git", branch: "main"
+
+  def install
+    # Install specific directories to libexec to ensure correct structure
+    libexec.install "bin", "lib", "README.md", "LICENSE"
+    
+    # Symlink the main binary from libexec/bin to the global bin directory
+    bin.install_symlink libexec/"bin/better-anonymity"
+    
+    # Create aliases pointing to the SAME target in libexec
+    bin.install_symlink libexec/"bin/better-anonymity" => "better-anon"
+    bin.install_symlink libexec/"bin/better-anonymity" => "b-a"
+  end
+
+  def caveats
+    <<~EOS
+      You can now run 'better-anonymity', 'better-anon', or 'b-a'.
+      
+      To start using the tool, run:
+        b-a
+        
+      To check your privacy score:
+        b-a diagnose
+    EOS
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/better-anonymity help")
+  end
+end
