e11613875c7f6aab9d921831c8d72ec4fe057459
John Patrick Roach <johnpatrickroach1@gmail.com>
2026-03-24 17:00:38 -0700

    Update URL to version 1.0.1 for better-anonymity

--- Formula/better-anonymity.rb
+++ Formula/better-anonymity.rb
@@ -1,7 +1,7 @@
 class BetterAnonymity < Formula
   desc "MacOS Security, Privacy & Anonymity Tools"
   homepage "https://github.com/johnpatrickroach/better-anonymity"
-  url "https://github.com/johnpatrickroach/better-anonymity/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/johnpatrickroach/better-anonymity/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   head "https://github.com/johnpatrickroach/better-anonymity.git", branch: "main"
